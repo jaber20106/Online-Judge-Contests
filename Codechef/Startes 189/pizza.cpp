aea8f36574bab716ce5b16732cffb32616ec1037
jaber20106 <azharulislamjaber20106@gmail.com>
2025-06-19 23:31:19 +0600

    code

---
+++ Codechef/Startes 189/pizza.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int x = (a+1) * 4;
+    int y = b * 3;
+    int sum = x + y;
+    int r = (sum+7) / 8;
+    cout<<r<<endl;
+
+}
